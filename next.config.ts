f82e4726e3bfea5a7da45696c3e7b08480e17cd7
hengkianggia <pengkyjd00@gmail.com>
2025-12-11 15:39:12 +0700

    add dockerfile, cicd deploy

--- next.config.ts
+++ next.config.ts
@@ -3,8 +3,14 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   devIndicators: false,
   output: "standalone",
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
   images: {
     domains: ["*"],
+  },
+  compiler: {
+    removeConsole: false,
   },
 };
 
